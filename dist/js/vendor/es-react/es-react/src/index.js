be8c3ca3a40e37ac418b9e6accae4320ec150223
Kynan Stewart <2020@mx.kynan.net.au>
2020-04-12 11:47:54 +1000

    Adding the dist vendor file to .gitignore

--- dist/js/vendor/es-react/es-react/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from './react';
-import ReactDOM from './react-dom';
-import PropTypes from './prop-types';
-
-export { React, ReactDOM, PropTypes };
-export * from './react';
-export default React;
