7447626f410655f832f1f5f00d01d330ba4d77fa
Kynan Stewart <2020@mx.kynan.net.au>
2020-04-26 20:40:07 +1000

    Prettier config and Emotion for CSS

--- js/App.js
+++ js/App.js
@@ -1,14 +1,19 @@
-import { h, render } from '../web_modules/preact.js'
-import { useState } from '../web_modules/preact/hooks.js'
-import Counter from './Counter.js';
-import htm from '../web_modules/htm.js'
-const html = htm.bind(h)
+import { h, render } from "../web_modules/preact.js";
+import { useState } from "../web_modules/preact/hooks.js";
+import { css } from "../web_modules/emotion.js";
+import Counter from "./Counter.js";
+import htm from "../web_modules/htm.js";
+const html = htm.bind(h);
+
+const someCssClass = css`
+  font-size: 2em;
+`;
 
 const App = props => {
-	return html`
-    <h1>Look Ma! No script tags, no build step</h1>
-    <${Counter} count=0 />
-  `
-}
+  return html`
+    <h1 className="${someCssClass}">Look Ma! No script tags, no build step</h1>
+    <${Counter} count="0" />
+  `;
+};
 
 export default App;
--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-import { h, hydrate } from '../web_modules/preact.js'
-import App from './App.js';
-import htm from '../web_modules/htm.js'
-const html = htm.bind(h)
+import { h, hydrate } from "../web_modules/preact.js";
+import App from "./App.js";
+import htm from "../web_modules/htm.js";
+const html = htm.bind(h);
 
 hydrate(
-	html`
+  html`
     <${App} />
   `,
-	document.getElementById('main-container')
-)
+  document.getElementById("main-container"),
+);
