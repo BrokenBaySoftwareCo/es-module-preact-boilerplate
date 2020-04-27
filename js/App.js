511e7845b2197dd3323ec13643e18aa9f8b7afdd
Kynan Stewart <2020@mx.kynan.net.au>
2020-04-27 22:46:51 +1000

    Started adding Tape. Switched from Emotion to SimpleStyle-JS for CSS

--- js/App.js
+++ js/App.js
@@ -1,17 +1,21 @@
 import { h, render } from "../web_modules/preact.js";
 import { useState } from "../web_modules/preact/hooks.js";
-import { css } from "../web_modules/emotion.js";
+import { createStyles } from "../web_modules/simplestyle-js.js";
 import Counter from "./Counter.js";
 import htm from "../web_modules/htm.js";
 const html = htm.bind(h);
 
-const someCssClass = css`
-  font-size: 2em;
-`;
+const [styles] = createStyles({
+  someCssClass: {
+    fontSize: "2em",
+  },
+});
 
 const App = props => {
   return html`
-    <h1 className="${someCssClass}">Look Ma! No script tags, no build step</h1>
+    <h1 className="${styles.someCssClass}">
+      Look Ma! No script tags, no build step
+    </h1>
     <${Counter} count="0" />
   `;
 };
